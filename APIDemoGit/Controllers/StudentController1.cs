531863268aeee6d6c023c46bc3cc61bb057791eb
ajeetpandey551 <ajeetpandey551@gmail.com>
2022-05-05 16:43:59 +0530

    Student commit

---
+++ APIDemoGit/Controllers/StudentController1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIDemoGit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController1 : ControllerBase
+    {
+        // GET: api/<StudentController1>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StudentController1>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentController1>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentController1>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController1>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
